1eae58aa5d08965ec32fd38d8077b60f1f3d6508
bonzstars <zengyuxing@yeezon.com>
2017-12-28 19:39:49 +0800

    gh-pages

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const ADD_NOTE = 'ADD_NOTE' // 添加笔记
-export const EDIT_NOTE = 'EDIT_NOTE' // 编辑笔记
-export const DELETE_NOTE = 'DELETE_NOTE' // 删除笔记
-export const DELETE_NOTE_ALL = 'DELETE_NOTE_ALL'
-export const SET_ACTIVE_NOTE = 'SET_ACTIVE_NOTE' // 设置当前活动笔记对象
-export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE' // 切换收藏
-export const TOGGLE_FILTER = 'TOGGLE_FILTER' //筛选列表
